4b1371c5b32829beb97d2fa51499ea56616457b7
kenosborne <ken.osborne.iii@gmail.com>
2020-12-15 09:53:10 -0800

    create joke component

---
+++ components/Joke.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="joke">
+        {{ joke }}
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Joke',
+    prop: ['joke', 'id']
+}
+</script>
+
+<style>
+.joke {
+    padding: 1rem;
+    border: 1px dotted #ccc;
+    margin: 1rem 0;
+}
+</style>
