b751dcf1e65e5e1616a3140b28a832044a689f1f
kenosborne <ken.osborne.iii@gmail.com>
2020-12-15 08:19:45 -0800

    create the about page

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>About DadJokes</h1>
+        <p>This is an app that displays dad jokes of all shapes and sizes!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About the app'
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: "We've cornered the dad joke market"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
