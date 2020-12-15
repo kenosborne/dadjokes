03d2443279d58bcdd8fb31b0971e51c9f11f7917
kenosborne <ken.osborne.iii@gmail.com>
2020-12-15 09:48:55 -0800

    get dad jokes from the api on the joke page

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -5,8 +5,42 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    
+    data() {
+        return {
+            jokes: []
+        }
+    },
+
+    async created() {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            this.jokes = res.data.results;
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    head() {
+        return {
+            title: 'Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: "We've cornered the dad joke market"
+                }
+            ]
+        }
+    }
 }
 </script>
 
